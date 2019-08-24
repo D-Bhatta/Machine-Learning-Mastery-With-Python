6ed0a66c6b45c1691622d80436522654d7364639
D-Bhatta <dbhatta1232@gmail.com>
2019-08-24 17:41:05 -0400

    Modify file for Lesson1_24_Aug_2019 as Lesson1_24_Aug_2019.py

--- Part_II/Lesson1/Lesson1_24_Aug_2019.py
+++ Part_II/Lesson1/Lesson1_24_Aug_2019.py
@@ -1,1 +1,26 @@
 #Lesson 1
+
+#page 20/179
+
+#scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+
+#numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+
+#matplotlib
+import matplotlib as pl
+print('pl: {}'.format(pl.__version__))
+
+#pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+
+#scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
+
+"Modify file for Lesson1_24_Aug_2019 as Lesson1_24_Aug_2019.py"
