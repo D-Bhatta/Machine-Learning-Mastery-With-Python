b440fd77103eecf67a448f0eb2b2fbb8e83cab80
D-Bhatta <dbhatta1232@gmail.com>
2019-09-03 13:59:38 -0400

    Modify file for Lesson5_2_Sep_2019 as Lesson5_2_Sep_2019.py

--- Part_II/Lesson5/Lesson5_2_Sep_2019.py
+++ Part_II/Lesson5/Lesson5_2_Sep_2019.py
@@ -17,31 +17,46 @@ def histogram():
     """Create a histogram of the data distribution of each variable"""
     data = load_data()
     data.hist()
-    pl.show()
+    pl.savefig("histogram.png", format = 'png')
     return "Histogram is plotted"
 
 def density_plots():
     """Create denisty plots of each variable in the distribution"""
     data = load_data()
     data.plot(kind = 'density', subplots = True, layout = (3,3), sharex = False)
-    pl.show()
+    pl.savefig("density_plots.png", format = 'png')
     return "Density Plot is plotted"
 
 def box_plots():
     """Create boxplots of each variable in the distribution"""
     data = load_data()
     data.plot(kind = 'box', subplots = True, layout = (3,3), sharex = False, sharey = False)
-    pl.show()
+    pl.savefig("box_plots.png", format = 'png')
     return "Box Plot is plotted"
+
 def correlation_matrix_plot():
     """Create a correlation matrix of all the variables"""
     import numpy
+    names = ['preg','plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
     data = load_data()
     correlations = data.corr()
     #plot correlation matrix
-    fig = pylot.figure()
+    fig = pl.figure()
     ax = fig.add_subplot(111)
     cax = ax.matshow(correlations, vmin = -1, vmax = 1)
-    fi.colorbar(cax)
+    fig.colorbar(cax)
     ticks = numpy.arange(0,9,1)
-    
+    ax.set_xticks(ticks)
+    ax.set_yticks(ticks)
+    ax.set_xticklabels(names)
+    ax.set_yticklabels(names)
+    pl.savefig("correlation_matrix_plot.png", format = 'png')#saves the plot
+    return "Correlation Matrix has been plotted"
+
+def scatter_plot():
+    """Create a Scatter Plot Matrix of all the variables"""
+    data = load_data()
+    from pandas.plotting import scatter_matrix
+    scatter_matrix(data)
+    pl.savefig("scatter_plot.png", format = 'png')#saves the plot
+    return "Scatter Plot matrix has been plotted"
