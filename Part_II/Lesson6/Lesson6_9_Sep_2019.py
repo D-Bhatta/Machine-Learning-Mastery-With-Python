53b6dbb507c3699343ed9d903fd6d59bcbc710b7
D-Bhatta <dbhatta1232@gmail.com>
2019-09-15 13:20:00 -0400

    Modify file for Lesson6_7_Sep_2019 as Lesson6_7_Sep_2019.py

--- Part_II/Lesson6/Lesson6_9_Sep_2019.py
+++ Part_II/Lesson6/Lesson6_9_Sep_2019.py
@@ -2,3 +2,51 @@
 #
 # Page : 56/179
 #
+import os
+from pandas import read_csv
+from numpy import set_printoptions
+
+os.chdir("J:\Education\Code\DATA_Science\Books\Jason_Brownlee\Machine-Learning-Mastery-With-Python\Part_II\Lesson6")
+
+def load_data():
+    filename = "pima-indians-diabetes.data.csv"
+    names = ['preg', 'plas', 'pres', 'skin', 'test' , 'mass', 'pedi', 'age', 'class']
+    dataframe = read_csv(filename, names = names)
+    array = dataframe.values
+    return array
+
+def rescale_data():
+    from sklearn.preprocessing import MinMaxScaler
+    array = load_data()
+    x = array[:,0:8]
+    y = array[:,8]
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    rescaledx = scaler.fit_transform(x)
+    return rescaledx,rescaledx[0:5,:]
+
+def strandardize_data():
+    from sklearn.preprocessing import StandardScaler
+    array = load_data()
+    x = array[:,0:8]
+    y = array[:,8]
+    scaler = StandardScaler().fit(x)
+    rescaledx = scaler.transform(x)
+    return rescaledx, rescaledx[0:5,:]
+
+def normalize_data():
+    from sklearn.preprocessing import Normalizer
+    array = load_data()
+    x = array[:,0:8]
+    y = array[:,8]
+    scaler = Normalizer().fit(x)
+    normalizedx = scaler.transform(x)
+    return normalizedx, normalizedx[0:5,:]
+
+def binarize_data():
+    from sklearn.preprocessing import Binarizer
+    array = load_data()
+    x = array[:,0:8]
+    y = array[:,8]
+    binarizer = Binarizer(threshold = 0.0).fit(x)
+    binaryx = binarizer.transform(x)
+    return binaryx, binaryx[0:5,:]
