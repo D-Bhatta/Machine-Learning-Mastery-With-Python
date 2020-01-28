cf5fcfe595750bf804c3a9a8b3a93c7bacd19146
D-Bhatta <dbhatta1232@gmail.com>
2020-01-28 13:23:14 -0500

    Create files and steup project for analysis of Boston Housing dataset
    README.md has been created to serve as the record for the project.
    boston.py is the main python script for the project.
    The data is stored in datasets/ folder.
    
    This project aims to analyse the Boston Housing dataset.

---
+++ Part_III/Lesson20/README.md
@@ -0,0 +1,80 @@
+# Boston Housing Dataset Classification
+
+This repository hosts the code, tests, documentation and pipelined model for the analysis undertaken of the Boston House Price dataset.
+
+## About the Dataset: Boston Housing
+
+The repository is hosted at [UCI Machine Learning Repository](https://archive.ics.uci.edu/ml/machine-learning-databases/housing/)
+
+The data set is multivariate and contains ratio(numerical) and nominal data.
+There are 506 instances and 14 attributes.
+
+## Project Steps
+
+1. [ ] Data Download
+2. [ ] Data Loading
+3. [ ] Data Analysis
+   1. [ ] Descriptive Statistics
+4. [ ] Data Visualization
+   1. [ ] Unimodal Data Visualizations
+   2. [ ] Multimodal Data Visualizations
+5. [ ] Partitioning of dataset into Training dataset and Validation dataset
+6. [ ] Model Creation
+7. [ ] Model Selection
+   - [ ] Create test harness using K-Fold Cross Validation, with scoring set to 'Negative Mean Squared Error'
+   - [ ] Evaluation of models using test harness
+   - [ ] Summarization, Visualization and Comparison of Results
+   - [ ] Re-evaluation of models using test harness
+   - [ ] Standardization of Dataset
+   - [ ] Summarization, Visualization and Comparison of Results
+   - [ ] Model Selection
+   - [ ] Model tuning
+   - [ ] Creation of Ensembles
+   - [ ] Evaluation of models using test harness
+   - [ ] Summarization, Visualization and Comparison of Results
+   - [ ] Tuning of Ensemble models
+8. [ ] Model Finalization
+   1. [ ] Model Preparation
+   2. [ ] Validation Set transformation
+   3. [ ] Summarization, Visualization and Comparison of Results
+   4. [ ] Training the model on entire dataset
+9. [ ] Summarization of Results
+10. [ ] Saving the Pipelined Project
+11. [ ] Testing the saved model
+
+## Project Files
+
+1. Dataset
+2. Python file (using template.py as the base)
+3. Model files
+4. Image files
+5. Documentation
+   1. README.md
+   2. Project Report
+   3. Slide deck
+
+## Images
+
+### Results
+
+### Tests
+
+<!--![Screenshot of Test Results](test_results.png "Test Results")-->
+<!-- The following things are tested:
+
+- Loading and partiioning of data
+- Accuracy of finalized model -->
+
+### Figures
+
+## Project Output
+
+    Output Here.
+
+## Future Steps
+
+Looking at how others have approached this project and writing a report on it
+
+## Project Status
+
+Project is currently in progress.
---
+++ Part_III/Lesson20/boston.py
@@ -0,0 +1,68 @@
+# Python Project Template
+# 1. Prepare Problem
+""" This step is about loading everything you need to start working on your problem. This is also the home of any global configuration you might need to do. It is also the place
+where you might need to make a reduced sample of your dataset if it is too large to work with. """
+# a) Load libraries
+# b) Load dataset
+# 2. Summarize Data
+""" This step is about better understanding the data that you have available. This includes
+understanding your data using
+
+- Descriptive statistics such as summaries.
+- Data visualizations such as plots with Matplotlib, ideally using convenience functions from
+Pandas.
+
+Take your time and use the results to prompt a lot of questions, assumptions and hypotheses
+that you can investigate later with specialized models. """
+# a) Descriptive statistics
+# b) Data visualizations
+# 3. Prepare Data
+""" This step is about preparing the data in such a way that it best exposes the structure of the
+problem and the relationships between your input attributes with the output variable.
+Start simple. Revisit this step often and cycle with the next step until you converge on a
+subset of algorithms and a presentation of the data that results in accurate or accurate-enough
+models to proceed. """
+# a) Data Cleaning
+# b) Feature Selection
+# c) Data Transforms
+# 4. Evaluate Algorithms
+""" This step is about finding a subset of machine learning algorithms that are good at exploiting
+the structure of your data (e.g. have better than average skill).
+On a given problem you will likely spend most of your time on this and the previous step
+until you converge on a set of 3-to-5 well performing machine learning algorithms. """
+# a) Split-out validation dataset
+# b) Test options and evaluation metric
+# c) Spot Check Algorithms
+# d) Compare Algorithms
+# 5. Improve Accuracy
+""" Once you have a shortlist of machine learning algorithms, you need to get the most out of them.
+The line between this and the previous step can blur when a project becomes concrete.
+There may be a little algorithm tuning in the previous step. And in the case of ensembles, you
+may bring more than a shortlist of algorithms forward to combine their predictions. """
+# a) Algorithm Tuning
+# b) Ensembles
+# 6. Finalize Model
+""" Once you have found a model that you believe can make accurate predictions on unseen data,
+you are ready to finalize it. """
+# a) Predictions on validation dataset
+# b) Create standalone model on entire training dataset
+# c) Save model for later use
+""" ### Tips for using the template
+
+- Fast First Pass . Make a first-pass through the project steps as fast as possible. This
+will give you confidence that you have all the parts that you need and a baseline from
+which to improve.
+- Cycles . The process in not linear but cyclic. You will loop between steps, and probably
+spend most of your time in tight loops between steps 3-4 or 3-4-5 until you achieve a level
+of accuracy that is sufficient or you run out of time.
+- Attempt Every Step . It is easy to skip steps, especially if you are not confident or
+familiar with the tasks of that step. Try and do something at each step in the process,
+even if it does not improve accuracy. You can always build upon it later. Don’t skip steps,
+just reduce their contribution.
+- Ratchet Accuracy . The goal of the project is model accuracy. Every step contributes
+towards this goal. Treat changes that you make as experiments that increase accuracy as
+the golden path in the process and reorganize other steps around them. Accuracy is a
+ratchet that can only move in one direction (better, not worse).
+- Adapt As Needed . Modify the steps as you need on a project, especially as you become
+more experienced with the template. Blur the edges of tasks, such as steps 4-5 to best
+serve model accuracy. """
